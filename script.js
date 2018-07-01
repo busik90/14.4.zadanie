884096720eaef65b6e55173a6ce6962ea4b2d0a1
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-01 19:51:17 +0200

    Add propTypes to classes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var MoviesList = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.array.isRequired
+  },
+
   render: function() {
     React.createElement('div', {},
       React.createElement('h1', {}, this.props.movie.listTitle),
@@ -8,24 +12,40 @@ var MoviesList = React.createClass({
 });
 
 var Movie = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.array.isRequired
+  },
+  
   render: function() {
     return React.createElement('li', {key: this.props.movie.id})
   }
 });
 
 var MovieTitle = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.array.isRequired
+  },
+  
   render: function() {
     return React.createElement('h2', {}, this.props.movie.title)
   }
 });
 
 var MovieDescription = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.array.isRequired
+  },
+  
   render: function() {
     return React.createElement('p', {}, this.props.movie.desc)
   }
 });
 
 var MovieCover = React.createClass({
+  propTypes: {
+    movie: React.PropTypes.array.isRequired
+  },
+  
   render: function() {
     return React.createElement('img', {src: "./images/" + this.props.movie.cover})
   }
