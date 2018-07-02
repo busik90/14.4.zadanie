2a0383b184512a0f3748054fff1332434dde00cd
Pawel Bus <pawelbus.dev@outlook.com>
2018-07-02 17:55:12 +0200

    Add table with movies

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+var movies = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'Film o czarodzieju',
+    cover: 'harry-potter.jpg'
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny',
+    cover: 'król-lew.jpg'
+  },
+  {
+    id: 3,
+    title: 'Atlas Chmur',
+    desc: 'Film o życiach ludzi w różnych czasach, różnych wcieleniach',
+    cover: 'atlas-chmur.jpg'
+  },
+  {
+    id: 4,
+    title: 'Colony',
+    desc: 'Serial o najeźdźcach z kosmosu',
+    cover: 'colony.jpg'
+  }
+];
+
 var MoviesList = React.createClass({
   propTypes: {
     movie: React.PropTypes.array.isRequired
